7808a0b3e36c76738ae81318e6e7b08f5d42151c
Wizardzaron <89619552+Wizardzaron@users.noreply.github.com>
2024-12-16 09:16:58 -0800

    Add files via upload
    Project I made when I was in school

---
+++ hello.py
@@ -0,0 +1,10 @@
+#Name: Ryan Haddadi
+#Date: 8/26/2021
+#File Purpose: Printing hello Function
+
+def helloworld():
+	return "Hello World!"
+
+def helloname(name):
+	return "Hello " + name + "!"
+
---
+++ main.py
@@ -0,0 +1,12 @@
+#Name : Ryan Haddadi
+#Date : 8/26/2021
+#File Purpose : Lab01 main program driver program
+
+from hello import *
+
+print("***TUFFY TITAN HELLO PROGRAM ***")
+
+print(helloworld())
+
+print(helloname("Steve"))
+
